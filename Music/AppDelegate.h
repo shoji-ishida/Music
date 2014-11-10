8c3c22d89faf23726db0d26d0c30cfadbd060ad7
Shoji Ishida <shoji.ishida@gmail.com>
2014-11-10 12:42:56 +0900

    Initial Commit

---
+++ Music/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Music
+//
+//  Created by 石田 勝嗣 on 2014/11/10.
+//  Copyright (c) 2014年 石田 勝嗣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Music/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  Music
+//
+//  Created by 石田 勝嗣 on 2014/11/10.
+//  Copyright (c) 2014年 石田 勝嗣. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
